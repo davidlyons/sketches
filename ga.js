846eb87127eb9c64cd2b07cfea45febeb4b3caf6
David Lyons <david.scott.lyons@gmail.com>
2020-02-11 19:06:32 -0800

    analytics

---
+++ ga.js
@@ -0,0 +1,10 @@
+
+if ( document.location.hostname != 'localhost' ) {
+
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-113315813-1');
+
+}
