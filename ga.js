bbd2a99cdd94b69577c6553dd7d14678c01453c9
David Lyons <david.scott.lyons@gmail.com>
2021-04-03 10:55:52 -0700

    ga4

--- ga.js
+++ ga.js
@@ -1,10 +1,8 @@
 
 if ( document.location.hostname != 'localhost' ) {
-
 	window.dataLayer = window.dataLayer || [];
 	function gtag(){dataLayer.push(arguments);}
 	gtag('js', new Date());
 
-	gtag('config', 'UA-113315813-1');
-
+	gtag('config', 'G-WTY198QNED');
 }
